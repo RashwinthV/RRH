7c0a8079e94a4981d168c22fe1f3a81ab7fef9e4
tharun <vrashwinth@gmail.com>
2024-10-06 20:07:14 +0530

    new

---
+++ Scripts/otp.js
@@ -0,0 +1,18 @@
+function genotp(){
+  return 6741;
+}
+function verifyOTP() {
+    const inputCode = [
+        document.getElementById('cod1').value,
+        document.getElementById('cod2').value,
+        document.getElementById('cod3').value,
+        document.getElementById('cod4').value
+    ].join('');
+let generatedOtp=genotp();
+    if (inputCode == generatedOtp) {
+        info("OTP Verified Successfully!");
+        window.location.href = "success.html";  // Redirect to success page
+    } else {
+        alert("Invalid OTP. Please try again.");
+    }
+}
